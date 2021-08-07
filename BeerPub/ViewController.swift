9181abe8713437bff5c0f6fda4682f480582fe8c
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-07 09:08:39 +0300

    Initial Commit

---
+++ BeerPub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerPub
+//
+//  Created by Евгений Березенцев on 07.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
